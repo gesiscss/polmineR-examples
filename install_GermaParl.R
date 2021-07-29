5273dc2724709a722ba03e6b91c211ff55527d5c
Mridul Seth <seth.mridul@gmail.com>
2021-07-29 17:17:27 +0200

    add install instructions for germapal

---
+++ install_GermaParl.R
@@ -0,0 +1,11 @@
+url <- "https://cran.r-project.org/src/contrib/Archive/GermaParl/GermaParl_1.5.2.tar.gz"
+pkgFile <- "GermaParl_1.5.2.tar.gz"
+download.file(url = url, destfile = pkgFile)
+
+install.packages(pkgs=pkgFile, type="source", repos=NULL)
+
+unlink(pkgFile)
+
+library(GermaParl)
+
+GermaParl::germaparl_download_corpus()
